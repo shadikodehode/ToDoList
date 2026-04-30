615a646c65c4b5b00c4e39425456160e9f1b506c
shadikodehode <shadi.kodehode@gmail.com>
2026-05-01 01:07:41 +0200

    ToDoList
    fixed themeProvider dependency and removing redundant useEffect

--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -1,15 +1,11 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { ThemeContext } from "./ThemeContext.jsx";
 import { useLocalStorage } from "../hooks/useLocalStorage.js";
 
 export default function ThemeProvider({ children }) {
   const [isDark, setIsDark] = useLocalStorage("isDark", false)
 
-  useEffect(() => {
-    localStorage.setItem("isDark", JSON.stringify(isDark))
-  }, [isDark])
-
-const value = useMemo(() => ({ isDark, setIsDark}), [isDark])
+const value = useMemo(() => ({ isDark, setIsDark}), [isDark, setIsDark])
 
   return (
     <ThemeContext.Provider value={value}>
