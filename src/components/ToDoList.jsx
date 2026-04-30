cdb4777330899c7e59985f77023fafc0b60abc5c
shadikodehode <shadi.kodehode@gmail.com>
2026-04-30 23:07:48 +0200

    ToDoList
    Cleaned up some prop drilling

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,7 +2,7 @@ import { useToDo } from "../context/ToDoContext.jsx"
 import ToDoItem from "./ToDoItem.jsx"
 
 export default function ToDoList() {
-  const { sortedData, editTask, deleteTask } = useToDo()
+  const { sortedData } = useToDo()
 
   if(sortedData.length === 0) {
     return <h3 className="p-8 -mt-4 font-semibold text-gray-700"> No notes added yet</h3>
@@ -10,7 +10,7 @@ export default function ToDoList() {
   return (
     <ul className="p-4 w-full">
       {sortedData.map(task => {
-        return <ToDoItem key={task.id} data={{ task, editTask, deleteTask }}/>
+        return <ToDoItem key={task.id} data={{ task }}/>
       })}
     </ul>
   )
